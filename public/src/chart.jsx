e65089515eb90a2be2d1641068eaddc050b18a20
Josh Brito <jbrito6492@gmail.com>
2020-08-07 23:06:30 -0700

    refactored seed.js
    I placed a random data generator function in exampleData.js and reserved seed.js specifically for seeding the db

--- public/src/chart.jsx
+++ public/src/chart.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Chart(props) {
   console.log(props.schools)
-    const listItems = props.schools.map(school => (
-      <li key={school._id}>
-        {school.name}
-      </li>
-    ))
+  const listItems = props.schools.map(school => (
+    <li key={school._id}>
+      {school.name}
+    </li>
+  ))
   return (
-  <ul>{listItems}</ul>
+    <ul><h3>Schools</h3>{listItems}</ul>
   )
 }
 
--- seed.js
+++ seed.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const seeder = require('mongoose-seed');
-const faker = require('faker');
+const exampleData = require('./exampleData.js');
 
 // Connect to MongoDB via Mongoose
 seeder.connect('mongodb://localhost/schools_db', { useUnifiedTopology: true }, function () {
@@ -13,7 +13,7 @@ seeder.connect('mongodb://localhost/schools_db', { useUnifiedTopology: true }, f
   // Clear specified collections
   seeder.clearModels(['School', 'Review'], function () {
     // Callback to populate DB once collections have been cleared
-    data = randomData();
+    var data = exampleData.dataGenerator();
     seeder.populateModels(data, function () {
       // Data array containing seed data - documents organized by Model
       seeder.disconnect();
@@ -21,86 +21,6 @@ seeder.connect('mongodb://localhost/schools_db', { useUnifiedTopology: true }, f
   });
 });
 
-// function to generate 300 fake reviews and 100 fake houses with random data points
-var randomData = () => {
-  // create random arrs
-  var verbs = [' Elementary School', ' High School', ' Middle School', ' Academy'];
-  var vIdx = Math.floor(Math.random() * 4);
-
-  var distanceArr = ['Nearby school', 'Serves this home'];
-  var networkArr = ['Public', 'Private']
-  var nIdx = Math.floor(Math.random() * 2);
-
-  var gradesArr = ['Preschool to 6', 'Preschool to 8', 'K to 12', '6 to 12', '9 to 12'];
-  var gIdx = Math.floor(Math.random() * 5);
-  // array to hold all school docs
-  var schoolRecords = [];
-
-  // array to hold all reviews
-  var reviewRecords = [];
-
-  // 100 school data documents
-  for (let i = 0; i < 100; i++) {
-    var schoolData = {
-      rating: [{
-        academicProgress: faker.random.number({ min: 0, max: 10 }),
-        lowIncome: faker.random.number({ min: 0, max: 10 }),
-        equity: faker.random.number({ min: 0, max: 10 }),
-        testScores: faker.random.number({ min: 0, max: 10 }),
-        average: faker.random.number({ min: 0, max: 10 })
-      }],
-      name: faker.name.findName() + verbs[vIdx],
-      district: faker.address.streetName() + ' School District',
-      studentBody: faker.random.number({ min: 500, max: 2500 }),
-      teacherBody: faker.random.number({ min: 100, max: 500 }),
-      teachStudRatio: faker.random.number({ min: 1, max: 10 }) + ':' + faker.random.number({ min: 1, max: 10 }),
-      distance: faker.random.number({ min: 0.1, max: 10 }),
-      address: [{
-        street: faker.random.number({ min: 1000, max: 9999 }) + ' ' + faker.address.streetName(),
-        city: faker.address.city(),
-        state: faker.address.stateAbbr(),
-        zip: faker.address.zipCode()
-      }],
-      details: [{
-        network: networkArr[nIdx],
-        grades: gradesArr[gIdx],
-        nearByOrServes: distanceArr[nIdx]
-      }],
-    }
-    schoolRecords.push(schoolData);
-  }
-  // 300 reviews
-  for (let j = 0; j < 300; j++) {
-    var reviewData =
-    {
-      rating: faker.random.number({ min: 0, max: 10 }),
-      comment: faker.lorem.paragraphs(),
-      submitter: 'Parent',
-      date: faker.date.past()
-    }
-    reviewRecords.push(reviewData);
-  }
-  // set data variable
-  var data = [{
-    'model': 'School',
-    'documents': schoolRecords
-  },
-  {
-    'model': 'Review',
-    'documents': reviewRecords
-  }]
-  return data;
-}
-
-// recursive fn to get ratio (helper fn)
-// const getRatio = (teacherBod, studentBod) => {
-//   for (let i = 2; i <= teacherBod; i++) {
-//     if ((teacherBod / i) % 1 === 0 && (studentBod / i) % 1 === 0) {
-//       return getRatio(teacherBod / i, studentBod / i);
-//     }
-//   }
-//   return `${teacherBod}:${studentBod}`;
-// }
 
 
 
