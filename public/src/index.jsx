0eddb2552f878a075aa3111feecc1c545d91d3a6
Josh Brito <jbrito6492@gmail.com>
2020-08-18 15:27:20 -0700

    changed README

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('schools'));
