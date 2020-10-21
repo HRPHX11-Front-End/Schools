3492c7e61e5e38b50660e5762dc25f1dcb9b919b
Josh Brito <jbrito6492@gmail.com>
2020-10-20 23:46:27 -0700

    refactoring seeding files for optimization
    creating classes and methods

--- server/database/seeding_files/reviewClass.js
+++ server/database/seeding_files/reviewClass.js
@@ -1,94 +1,24 @@
-const faker = require('faker');
 
-const School = (gradesServed = null, schoolType = null, distance = null, network = null) => {
-  this.gradesServed = gradesServed;
-  this.schoolType = schoolType;
-  this.distance = distance;
-  this.network = network
-  this.academicProgress = faker.random.number({ min: 0, max: 10 });
-  this.lowIncome = faker.random.number({ min: 0, max: 10 });
-  this.equity = faker.random.number({ min: 0, max: 10 });
-  this.testScores = faker.random.number({ min: 0, max: 10 });
+const random = Math.floor(Math.random() * 10 + 1);
+const Review = (comment = null, date = null, rating= null) => {
+  this.rating = rating
+  this.comment = comment;
+  this.submitter = 'Parent';
+  this.date = date;
 }
-
-School.prototype.generateSchoolRecord = function () {
-  const gradesServed = ['Preschool to 6', 'Preschool to 8', 'K to 12', '6 to 12', '9 to 12'];
-  const schoolType = [' Elementary School', ' High School', ' Middle School', ' Academy'];
-  const distance = ['Nearby school', 'Serves this home'];
-  const network = ['Public', 'Private'];
-  this.gradesServed = getRandomElement(gradesServed);
-  this.schoolType = getRandomElement(schoolType);
-  this.distance = getRandomElement(distance);
-  this.network = getRandomElement(network);
-  return {
-    rating: [{
-      academicProgress: this.academicProgress,
-      lowIncome: this.lowIncome,
-      equity: this.equity,
-      testScores: this.testScores,
-      average: getAvg(this.academicProgress, this.lowIncome, this.equity, this.testScores),
-      lastUpdated: getDate(),
-    }],
-    name: faker.name.findName() + this.gradesServed,
-    district: faker.address.streetName() + ' School District',
-    studentBody: faker.random.number({ min: 500, max: 2500 }),
-    teacherBody: faker.random.number({ min: 100, max: 500 }),
-    studTeachRatio: getRatio(faker.random.number({ min: 100, max: 150 }), faker.random.number({ min: 20, max: 80 })),
-    distance: faker.random.number({ min: 0.1, max: 10 }),
-    address: [{
-      street: faker.random.number({ min: 1000, max: 9999 }) + ' ' + faker.address.streetName(),
-      city: faker.address.city(),
-      state: faker.address.stateAbbr(),
-      zip: faker.address.zipCode()
-    }],
-    details: [{
-      network: this.network,
-      grades: this.gradesServed,
-      nearByOrServes: this.distance
-    }],
-    reviews: reviews(),
-  }
-}
-
-const getRandomElement = (array) => {
-  const randomElement = Math.floor(Math.random() * array.length);
-  return array[randomElement];
-}
-
-module.exports.dataGenerator = () => {
-  // create random arrs
-
-  // array to hold all school docs
-  var schoolRecords = [];
-
-  // 100 school data documents
-  for (let i = 0; i < 100; i++) {
-
-    schoolRecords.push(schoolData);
-  }
-
-  var data = [{
-    'model': 'School',
-    'documents': schoolRecords
-  }]
-  return data;
-}
-
-// fn to obtain reviews
-const reviews = () => {
-  // create random variable
-  var random = Math.floor(Math.random() * 10 + 1);
-  // array to hold all reviews
-  var reviewRecords = [];
-  [...Array(random)].map((item) => {
-    reviewRecords.push({
-      rating: faker.random.number({ min: 1, max: 5 }),
-      comment: reviewGenerator(),
-      submitter: 'Parent',
-      date: getDate()
-    })
+// create random variable
+let rating = faker.random.number({ min: 1, max: 5 });
+// array to hold all reviews
+var reviewRecords = [];
+[...Array(random)].map((item) => {
+  reviewRecords.push({
+    rating: faker.random.number({ min: 1, max: 5 }),
+    comment: reviewGenerator(),
+    submitter: 'Parent',
+    date: getDate()
   })
-  return reviewRecords;
+})
+return reviewRecords;
 }
 
 // recursive fn to get ratio (helper fn)
@@ -143,4 +73,3 @@ const reviewGenerator = () => {
   })
   return review.join('');
 }
-
