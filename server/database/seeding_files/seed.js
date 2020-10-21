d6c9f06fb1e35f197f48abe06d32e6d2c48ef824
Josh Brito <jbrito6492@gmail.com>
2020-10-21 15:51:52 -0700

    refactored to work with Docker

--- server/database/seeding_files/seed.js
+++ server/database/seeding_files/seed.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const seeder = require('mongoose-seed');
 const helperFunctions = require('./helperFunctions.js');
 
-seeder.connect('mongodb://localhost:27017/schools_db', { useUnifiedTopology: true, useNewUrlParser: true }, function () {
+seeder.connect('mongodb://mongo:27017/schools_db', { useUnifiedTopology: true, useNewUrlParser: true }, function () {
 
   seeder.loadModels([
     path.join(__dirname, '..', 'models', 'schools.js'),
